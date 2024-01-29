a97347fb8164171d4c197dc84ff89a411cb9d86a
Jovius <joviuslim@hotmail.com>
2024-01-29 11:23:51 +0800

    added cart function (it actually works well just the database)

--- server.php
+++ server.php
@@ -12,12 +12,23 @@ if ($conn -> connect_error) {
 }
 
 // Handling cart
-if ($_SERVER["REQUEST_METHOD"] == "POST" & isset($_POST["add_to_cart"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" & isset($_POST["product_id"])) {
     $productId = $_POST['product_id'];
     $productName = $_POST['product_name'];
     $productPrice = $_POST['product_price'];
     $productQuantity = $_POST['product_quantity'];
 
-    $sql = 'INSERT INTO tempcart (id, name, price, quantity) VALUES ($productID, $productName, $productPrice)';
-    $result = $conn -> query($sql);
+    $sql = 'SELECT id FROM tempcart WHERE id = ?';
+    $result = $conn -> execute_query($sql, [$productId]);
+    $exists = $result -> num_rows;
+
+    if ($exists) {
+        $sql = 'UPDATE tempcart SET quantity = ?+quantity';
+        $result = $conn -> execute_query($sql, [$productQuantity]);
+        echo "UPDATE";
+    } else {
+        $sql = 'INSERT INTO tempcart (id, name, price, quantity) VALUES (?, ?, ?, ?)';
+        $result = $conn -> execute_query($sql, [$productId, $productName, $productPrice, $productQuantity]);
+        echo "INSERT";
+    }
 }
